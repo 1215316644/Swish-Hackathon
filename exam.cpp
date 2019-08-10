1484e85339b6ed52faad9498c373845cd21b709f
1215316644 <53968199+1215316644@users.noreply.github.com>
2019-08-10 15:33:56 +0530

    Add files via upload

---
+++ exam.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int profit;
+    float x,profit1;
+    cin>>profit>>profit1;
+    p1 = (profit1/100);
+    int cost=profit;
+    while(p>0){
+        x=profit*profit1;
+        profit=int(profit-x);
+        cost =cost + profit;
+    }
+    cout<<c<<endl;
+}
